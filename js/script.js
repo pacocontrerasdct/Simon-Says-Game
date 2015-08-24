e876e9a6541bab0a9f6636c6f2beedde0563ad30
Paco Contreras <pacocontrerasdct@gmail.com>
2015-08-24 22:06:12 +0100

    Countdown and new player functions working

--- js/script.js
+++ js/script.js
@@ -35,9 +35,8 @@ function setUp(){
   $('#parr1').append('Welcome to <em>Simon On -Line</em>, an online game based in an electronic game of memory skill invented by Ralph H. Baer and Howard J. Morrison, with software programming by Lenny Cope. Simon was launched in 1978 at Studio 54 in New York City and was an immediate success, becoming a pop culture symbol of the 1970s and 1980s.');
   $('#parr2').append('Instructions to play: The device has four colored buttons, each producing a particular tone when it is pressed or activated by the device. A round in the game consists of the device lighting up one or more buttons in a random order, after which the player must reproduce that order by pressing the buttons. As the game progresses, the number of buttons to be pressed increases.');
   $('#startButton').append('START').on('click', buildingBoard);
-  $('#quitButton').append('QUIT').on('click', quitGame);
+  $('#quitButton').append('QUIT').on('click', quit);
 
- 
 
 
   // Var numX pass a number to randomizer funct to get a max num of values for a Level of dificulty 
@@ -80,7 +79,7 @@ function doBlink(index, element){
 
 
 // Building Squares, or in others words css properties for colored divs
-function SquareFactory(backgroundColor, opacity, width, height, margin, padding, display, borderRadius, mozBorderRadius, webkitBorderRadius, dataBanana){
+function SquareFactory(backgroundColor, opacity, width, height, margin, padding, display, borderRadius, mozBorderRadius, webkitBorderRadius){
   this.backgroundColor = backgroundColor;
   this.opacity = opacity;
   this.height = height;
@@ -91,16 +90,40 @@ function SquareFactory(backgroundColor, opacity, width, height, margin, padding,
   this.borderRadius = borderRadius;
   this.mozBorderRadius = mozBorderRadius;
   this.webkitBorderRadius = webkitBorderRadius;
-  this.dataBanana = dataBanana;
 };
 // New player function with Name, currentlevel, startTime from the beginning of game, record is a combination between levels passed and total time spent to pass them, playerPattern is the recording of the last pattern player did (helps to check with randomPattern)
-function NewPlayer(name, level, record){
-  this.name = name;
-  this.level = level;
-  this.startTime = startTime;
-  this.record = record;
+function newPlayer(playerN){ 
+  this.name = playerN;
+  this.level = 0;
+  this.startTime = $.now(); // I'll need to sustract time from countdown
+  this.record = 0;
   this.playerPattern = playerPattern;
 };
+
+function playerName(){
+    // After showing board ask to player for a name
+  console.log('hello playerName');
+  alert('can get message on line 107');
+  $('aside').html('<p id="giveMeName"></p>');
+  $('#giveMeName').html('To start the game I will need your name, so I can keep your achievements updated in our Hall Of Fame.');
+  
+
+
+  // Building a input area and a button to get player name
+  $('aside').html('<input id="inputName" type="text" placeholder="Enter a name"/><button id="getNameButton" type="button">Get me!');
+  // On 'click' getting name
+  $('#getNameButton').on('click',function(){
+    inputFromPlayer = $('#inputName')[0];
+    console.log(inputFromPlayer.value);
+    playerN = inputFromPlayer.value;
+    // Creating a new player
+    playerId = new newPlayer(playerN); // I'll need to create an object to keep
+                                    //all different players while the game is running  
+  });
+  countDown();
+}
+
+
 
 function recordingPLayerPattern(){
   // Each square has a dataset number as Id so I'm collect it now
@@ -131,10 +154,10 @@ function buildingBoard(){
   // Giving css attributes to the container and squares using a Square Factory:
   // (backgroundColor, width, height, margin, padding, display, borderRadius, mozBorderRadius, webkitBorderRadius)
   squareContainer = new SquareFactory('rgb(0,0,0)', '1', '200px', '200px', '50px auto', '2px', 'block', '200px', '200px', '200px');
-  squareBlue = new SquareFactory('rgb(0,0,255)', '0.7', '100px', '100px', '0', '0', 'inline-block', '100px 0 50px 0', '100px 0 50px 0', '100px 0 50px 0', '555');
-  squareYellow = new SquareFactory('rgb(255,255,0)', '0.7', '100px', '100px', '0', '0', 'inline-block', '0 100px  0 50px', '0 100px  0 50px', '0 100px  0 50px', '2');
-  squareRed = new SquareFactory('rgb(255,0,0)', '0.7', '100px', '100px', '0', '0', 'inline-block', '0 50px 0 100px', '0 50px 0 100px', '0 50px 0 100px', '3');
-  squareGreen = new SquareFactory('rgb(0,255,0)', '0.7', '100px', '100px', '0', '0', 'inline-block', '50px 0 100px 0', '50px 0 100px 0', '50px 0 100px 0', '4');
+  squareBlue = new SquareFactory('rgb(0,0,255)', '0.7', '100px', '100px', '0', '0', 'inline-block', '100px 0 50px 0', '100px 0 50px 0', '100px 0 50px 0');
+  squareYellow = new SquareFactory('rgb(255,255,0)', '0.7', '100px', '100px', '0', '0', 'inline-block', '0 100px  0 50px', '0 100px  0 50px', '0 100px  0 50px');
+  squareRed = new SquareFactory('rgb(255,0,0)', '0.7', '100px', '100px', '0', '0', 'inline-block', '0 50px 0 100px', '0 50px 0 100px', '0 50px 0 100px');
+  squareGreen = new SquareFactory('rgb(0,255,0)', '0.7', '100px', '100px', '0', '0', 'inline-block', '50px 0 100px 0', '50px 0 100px 0', '50px 0 100px 0');
 
   console.log(squareContainer);
   
@@ -152,27 +175,31 @@ function buildingBoard(){
   $('#sqr3').css(squareRed).on('click', recordingPLayerPattern);
   $('#sqr4').css(squareGreen).on('click', recordingPLayerPattern);
 
-  countDown();
+  playerName();
 
 };
 
+// Countdown to start the game when player enter his/her name
 function countDown(){
-  countDownNumbers = ['1','2','3'];
-  $('aside').html('<p id="parr3">');
+  countDownNumbers = ['1','2','3','4']; 
   $.each(countDownNumbers, function (index, element){
-
-    $('#parr3').append(element).delay(1000 * index).fadeTo(300, 1);
-    $('#parr3').remove(element).delay(1000 * index).fadeTo(300, 0);
-    console.log(element);
+    spanTag = $('aside').append('<span id="span' + element + '">');
   });
+ $('#span1').append(countDownNumbers[2] + '... ').delay(500).fadeTo(200, 0);
+ $('#span2').append(countDownNumbers[1] + '... ').delay(1000).fadeTo(200, 0);
+ $('#span3').append(countDownNumbers[0] + '... ').delay(1500).fadeTo(200, 0);
+ $('#span4').append('Go!').fadeTo(0, 0).delay(2000).fadeTo(200, 1).delay(1000).fadeTo(200, 0);
+
+
+
 };
 
 
-
-function quitGame(){
+// This function close the window and finishes the game
+function quit(){
   url = "https://www.google.co.uk/";
-    var newWindow = window.open('', '_self', ''); //open the current window
-    window.close(url);
+  var newWindow = window.open('', '_self', ''); //open the current window
+  window.close(url);
 };
 
 
